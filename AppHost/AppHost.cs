1e1a1a94cf1a3e0a54733d8cd559b11c820cdd49
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2026-04-08 16:05:40 +0100

    Added AppHost Aspire project with its dependencies

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -2,4 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // add projects and cloud-native backing services here
 
+builder.AddProject<Projects.Catalog>("catalog");
+
+// add projects and cloud-native backing services here
+
 builder.Build().Run();
